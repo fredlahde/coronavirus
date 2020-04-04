100a44db576ef7f0f51dde62982c760bcbcb2566
Fred Lahde <frederick.lahde@worldiety.de>
2020-04-05 00:45:18 +0200

    update gen.sh

--- data-analysis/gen.sh
+++ data-analysis/gen.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 countries=(Germany US Italy Spain China Austria Sweden)
 mkdir -p png-rendered
-cd COVID-19 && git pull origin master && cd ../
+#cd COVID-19 && git pull origin master && cd ../
 
+ls
+pwd
 for c in ${countries[@]}; do
     python3 analyze.py "$c" --log --png --grid
 done
